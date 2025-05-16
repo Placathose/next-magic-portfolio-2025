22b2f8caa07945b17864ef817b65457606d4e4bf
Placathose <bruceplacathose@gmail.com>
2025-05-16 19:15:10 -0400

    Deploy: prepare for GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: "/next-magic-portfolio-2025",
+  assetPrefix: "/next-magic-portfolio-2025",
   output: "export",
   reactStrictMode: true,
 };
--- src/components/caroussel-projects.tsx
+++ src/components/caroussel-projects.tsx
@@ -1,36 +1,3 @@
-// import * as React from "react"
- 
-// import { Card, CardContent } from "@/components/ui/card"
-// import {
-//   Carousel,
-//   CarouselContent,
-//   CarouselItem,
-//   CarouselNext,
-//   CarouselPrevious,
-// } from "@/components/ui/carousel"
- 
-// export function CarouselSpacing() {
-//   return (
-//     <Carousel className="w-full max-w-3xl mx-auto">
-//       <CarouselContent className="-ml-1">
-//         {Array.from({ length: 5 }).map((_, index) => (
-//           <CarouselItem key={index} className="pl-1 md:basis-1/2 lg:basis-1/3">
-//             <div className="p-1">
-//               <Card>
-//                 <CardContent className="flex aspect-square items-center justify-center p-6 bg-red-700 w-">
-//                   <span className="text-2xl font-semibold">{index + 1}</span>
-//                 </CardContent>
-//               </Card>
-//             </div>
-//           </CarouselItem>
-//         ))}
-//       </CarouselContent>
-//       <CarouselPrevious />
-//       <CarouselNext />
-//     </Carousel>
-//   )
-// }
-
 import * as React from "react"
 import { Card, CardContent } from "@/components/ui/card"
 import {
