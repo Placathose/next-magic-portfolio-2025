48428d81cd1fd06b70e74ac12a00e5e5a40ce568
Placathose <bruceplacathose@gmail.com>
2025-05-16 19:52:40 -0400

    no more white screen

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: "/next-magic-portfolio-2025",
-  assetPrefix: "/next-magic-portfolio-2025",
-  output: "export",
   reactStrictMode: true,
 };
 
