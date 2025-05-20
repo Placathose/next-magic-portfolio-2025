518d68f27e2ed5ec5cfbcfd6cbabdb04bdba63d4
Placathose <bruceplacathose@gmail.com>
2025-05-20 14:29:05 -0400

    projects on top

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -56,6 +56,32 @@ export default function Page() {
         </BlurFade>
       </section>
 
+      <section id="projects">
+        <div className="space-y-12 w-full py-12 md-5">
+          <BlurFade delay={BLUR_FADE_DELAY * 11}>
+            <div className="flex flex-col items-center justify-center space-y-4 text-center">
+              <div className="space-y-2">
+                <div className="inline-block rounded-lg bg-foreground text-background px-3 py-1 text-sm">
+                  My Projects
+                </div>
+                <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
+                  Check out my latest work
+                </h2>
+                <p className="text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
+                  I&apos;ve worked on a variety of projects, from simple
+                  websites to complex web applications. Here are a few of my
+                  favorites.
+                </p>
+              </div>
+            </div>
+          </BlurFade>
+
+          <div className="mx-auto">
+            <CarouselProjects/>
+          </div>
+        </div>
+      </section>
+
       <section id="work" className="max-w-3xl mx-auto px-6">
         <div className="flex min-h-0 flex-col gap-y-3">
           <BlurFade delay={BLUR_FADE_DELAY * 5}>
@@ -129,52 +155,13 @@ export default function Page() {
 
         </div>
       </section>
-      <section id="projects">
-        <div className="space-y-12 w-full py-12 md-5">
-          <BlurFade delay={BLUR_FADE_DELAY * 11}>
-            <div className="flex flex-col items-center justify-center space-y-4 text-center">
-              <div className="space-y-2">
-                <div className="inline-block rounded-lg bg-foreground text-background px-3 py-1 text-sm">
-                  My Projects
-                </div>
-                <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
-                  Check out my latest work
-                </h2>
-                <p className="text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
-                  I&apos;ve worked on a variety of projects, from simple
-                  websites to complex web applications. Here are a few of my
-                  favorites.
-                </p>
-              </div>
-            </div>
-          </BlurFade>
-      
-          {/* <div className="grid grid-cols-1 gap-3 sm:grid-cols-2 max-w-[800px] mx-auto">
-              {DATA.projects.map((project, id) => (
-                <BlurFade
-                  key={project.title}
-                  delay={BLUR_FADE_DELAY * 12 + id * 0.05}
-                >
-                    <ProjectCard
-                    href={project.href}
-                    key={project.title}
-                    title={project.title}
-                    description={project.description}
-                    dates={project.dates}
-                    tags={project.technologies}
-                    image={project.image}
-                    video={project.video}
-                    links={project.links}
-                    />
 
-                </BlurFade>
-              ))}
-          </div> */}
+      <section id="contact" className="max-w-3xl mx-auto my-0 px-6">
+        <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
+          Some of my experiments
 
-          <div className="mx-auto">
-            <CarouselProjects/>
-          </div>
-        </div>
+          
+        </h2>
       </section>
     
       <section id="contact" className="max-w-3xl mx-auto my-0 px-6">
